96fc58d69db60d19e4c715e4bc6430d3bc10542b
Atik471 <108218772+Atik471@users.noreply.github.com>
2025-01-24 18:23:00 +0600

    added payment method

---
+++ src/components/CheckoutForm.jsx
@@ -0,0 +1,95 @@
+import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
+import { useContext } from "react";
+import { AuthContext } from "../contexts/AuthProvider";
+import PropTypes from "prop-types";
+import axios from "axios";
+import { LocationContext } from "../contexts/LocationProvider";
+
+const CheckoutForm = ({ clientSecret }) => {
+  const stripe = useStripe();
+  const elements = useElements();
+  const { user } = useContext(AuthContext);
+  const API = useContext(LocationContext);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    if (!stripe || !elements) {
+      return;
+    }
+
+    const card = elements.getElement(CardElement);
+
+    if (card == null) {
+      return;
+    }
+
+    const { error, paymentMethod } = await stripe.createPaymentMethod({
+      type: "card",
+      card,
+    });
+
+    if (error) {
+      console.log("[error]", error);
+    } else {
+      console.log("[PaymentMethod]", paymentMethod);
+    }
+
+    try {
+      const { paymentIntent, error: confirmError } =
+        await stripe.confirmCardPayment(clientSecret, {
+          payment_method: {
+            card: card,
+            billing_details: {
+              email: user?.email,
+              name: user?.displayName,
+            },
+          },
+        });
+
+      if (confirmError) {
+        console.log("Payment confirmation failed:", confirmError);
+      } else {
+        console.log("Payment successful:", paymentIntent);
+        axios.put(`${API}/update-badges`, {
+            userId: user.objectId,
+          })
+          .then(res => console.log(res))
+          .catch(e => console.log(e))
+      }
+    } catch (error) {
+      console.log("Unexpected error:", error);
+    }
+    
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <CardElement
+        options={{
+          style: {
+            base: {
+              fontSize: "16px",
+              color: "#424770",
+              "::placeholder": {
+                color: "#aab7c4",
+              },
+            },
+            invalid: {
+              color: "#9e2146",
+            },
+          },
+        }}
+      ></CardElement>
+      <button type="submit" disabled={!stripe}>
+        Pay
+      </button>
+    </form>
+  );
+};
+
+CheckoutForm.propTypes = {
+  clientSecret: PropTypes.string,
+};
+
+export default CheckoutForm;
--- src/components/Membership.jsx
+++ src/components/Membership.jsx
@@ -1,10 +1,38 @@
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+import CheckoutForm from "./CheckoutForm";
+import { useContext, useEffect, useState } from "react";
+import { LocationContext } from "../contexts/LocationProvider";
+
+const stripePromise = loadStripe(import.meta.env.VITE_pk_stripe);
 
 const Membership = () => {
-    return (
-        <div className="min-h-screen md:px-[5%] px-4 md:py-10 py-5">
-            Membership
-        </div>
-    );
+  const [clientSecret, setClientSecret] = useState("");
+  const API = useContext(LocationContext);
+
+  useEffect(() => {
+    fetch(`${API}/create-payment-intent`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ amount: 2500 }),
+      })
+        .then((res) => res.json())
+        .then((data) => setClientSecret(data.clientSecret))
+  }, [API]);
+
+
+  return (
+    <div className="min-h-screen md:px-[5%] px-4 md:py-10 py-5">
+      {clientSecret && (
+        <Elements
+          options={{ clientSecret }}
+          stripe={stripePromise}
+        >
+          <CheckoutForm clientSecret={clientSecret} />
+        </Elements>
+      )}
+    </div>
+  );
 };
 
 export default Membership;
