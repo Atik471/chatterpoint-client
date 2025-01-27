d2d939e81a8d3ba07b646903f88f3b839c6a1794
Atik471 <108218772+Atik471@users.noreply.github.com>
2025-01-27 15:17:40 +0600

    added pagination to activites page

--- src/components/Activities.jsx
+++ src/components/Activities.jsx
@@ -8,17 +8,20 @@ export let refetchReports;
 
 const Activities = () => {
   const API = useContext(LocationContext);
+  const [page, setPage] = useState(1);
+  // eslint-disable-next-line no-unused-vars
+  const [limit, setLimit] = useState(5);
   const [isLoading, setIsLoading] = useState(false);
 
   const fetchReports = async () => {
     setIsLoading(true);
-    const { data } = await axios.get(`${API}/report`);
+    const { data } = await axios.get(`${API}/report?page=${page}&limit=${limit}`);
     setIsLoading(false);
     return data;
   };
 
   const { data, isError, refetch } = useQuery({
-    queryKey: ["report"],
+    queryKey: ["report", page],
     queryFn: fetchReports,
     keepPreviousData: true,
   });
@@ -27,6 +30,16 @@ const Activities = () => {
   if (isError) return <div>Error loading posts.</div>;
 
   refetchReports = refetch;
+
+  const handlePageChange = async (newPage) => {
+    setIsLoading(true);
+    if (newPage < 1 || newPage > data.totalPages) return;
+    setPage(newPage);
+    await refetch();
+    setIsLoading(false);
+  };
+
+  const reports = data?.reports;
 
   return (
     <div>
@@ -40,11 +53,46 @@ const Activities = () => {
           </tr>
         </thead>
         <tbody>
-          {data?.map((report, index) => (
+          {reports?.map((report, index) => (
             <ActivityRow key={index} report={report} />
           ))}
         </tbody>
       </table>
+
+      <div className="pagination flex items-center justify-center gap-4 mt-6">
+        <button
+          onClick={() => handlePageChange(page - 1)}
+          disabled={page === 1}
+          className="px-4 py-2 bg-gray-800 text-white rounded-md shadow-md hover:bg-gray-700 disabled:bg-gray-500 disabled:cursor-not-allowed transition"
+        >
+          Prev
+        </button>
+
+        {Array.from(
+          { length: data?.totalPages || 1 },
+          (_, index) => index + 1
+        ).map((pageNumber) => (
+          <button
+            key={pageNumber}
+            onClick={() => handlePageChange(pageNumber)}
+            className={`px-3 py-1 text-lg font-semibold rounded-md shadow-sm ${
+              pageNumber === page
+                ? "bg-tertiary text-white transition-all duration-300"
+                : "bg-gray-200 text-gray-800 hover:bg-gray-300"
+            }`}
+          >
+            {pageNumber}
+          </button>
+        ))}
+
+        <button
+          onClick={() => handlePageChange(page + 1)}
+          disabled={page === data?.totalPages}
+          className="px-4 py-2 bg-gray-800 text-white rounded-md shadow-md hover:bg-gray-700 disabled:bg-gray-500 disabled:cursor-not-allowed transition"
+        >
+          Next
+        </button>
+      </div>
     </div>
   );
 };
